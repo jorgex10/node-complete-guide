149c981664ccd1fc95d8f5fdb1140f45e2776120
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-01-19 18:30:34 -0500

    Adding Cart model and logic to it

---
+++ models/cart.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+const mainPath = require("../util/path");
+
+const p = path.join(mainPath, "data", "cart.json");
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    // Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (product) => product.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+
+      // Add new product or increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.quantity = updatedProduct.quantity + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, quantity: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+
+      cart.totalPrice = cart.totalPrice + +productPrice;
+
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
