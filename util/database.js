f8933be058799bb2971bca2e81c3b38629849e34
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-01-22 12:15:45 -0500

    Adding and configuring sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "nuttertools",
-});
+const sequelize = new Sequelize(
+  "node-complete",
+  process.env.DB_name,
+  process.env.DB_PASSWORD,
+  { dialect: "mysql", host: "localhost" }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
