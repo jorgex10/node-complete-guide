5eeca56a4c72e76492d555fb9db11c1b579bf789
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-01-25 14:05:03 -0500

    Adding show products functionality

--- models/product.js
+++ models/product.js
@@ -1,6 +1,4 @@
-// const Sequelize = require("sequelize");
-
-// const sequelize = require("../util/database");
+const mongodb = require("mongodb");
 
 const getDb = require("../util/database").getDb;
 
@@ -32,6 +30,21 @@ class Product {
       .then((products) => {
         console.log(products);
         return products;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+
+        return product;
       })
       .catch((err) => console.log(err));
   }
