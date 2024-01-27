845de00e7c2ef977d9c134c62b33434ed4630a68
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-01-27 14:05:43 -0500

    Adding getProducts and index for products

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,9 @@ const express = require("express");
 const shopController = require("../controllers/shop");
 const router = express.Router();
 
-// router.get("/", shopController.getIndex);
+router.get("/", shopController.getIndex);
 
-// router.get("/products", shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
 // router.get("/products/:productId", shopController.getProduct);
 
