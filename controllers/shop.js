22b838d6117edff1a1a8473e4156a17694cd5a76
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-01-26 22:37:12 -0500

    Adding Orders functionalities with mongoDB

--- controllers/shop.js
+++ controllers/shop.js
@@ -77,32 +77,9 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-
-      return cart.getProducts();
-    })
-    .then((products) => {
-      return req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
-    })
-    .then(() => {
-      return fetchedCart.setProducts(null);
-    })
-    .then(() => {
+    .addOrder()
+    .then((result) => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
@@ -110,11 +87,11 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ["products"] })
+    .getOrders()
     .then((orders) => {
       res.render("shop/orders", {
-        pageTitle: "Checkout",
-        path: "/checkout",
+        pageTitle: "Your Orders",
+        path: "/orders",
         orders: orders,
       });
     })
