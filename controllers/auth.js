419225ecf0a654e65fe8b42ebee4ecba5e338736
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-02-01 19:06:24 -0500

    Adding signup page, routes and controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,6 +25,16 @@ exports.postLogin = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Signup",
+    path: "/signup",
+    isAuthenticated: false,
+  });
+};
+
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
