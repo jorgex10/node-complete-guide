784a96e3712a93360d29fe2991fe7480c3b3ec26
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-03-01 13:12:31 -0500

    Installing compression

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
 const helmet = require("helmet");
+const compression = require("compression");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -57,6 +58,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(helmet());
+app.use(compression());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
